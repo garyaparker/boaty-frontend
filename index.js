89347a2b850d5adbecd4da1eee444a0808015dd5
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-15 06:20:29 -0400

    Added redirect to HTTPS.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
 const app = express();
+
+// Trust the x-forwarded headers set by the ELB
+app.set('trust proxy', true);
+
+// Redirect HTTP to HTTPS
+app.get('*', (req, res, next) => {
+  req.protocol === 'http' ?
+    res.redirect('https://' + req.hostname + req.url) :  
+    next();
+});
+
 app.use((req, res) => res.send('hello boatz'));
 
 app.listen(process.env.PORT || 3000);
