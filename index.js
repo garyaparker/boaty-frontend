825d3103bfb2d762389e61833815c11267f756ba
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-15 13:49:26 -0400

    Fix travis.

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const app = express();
 app.set('trust proxy', true);
 
 // Redirect HTTP to HTTPS
-// app.get('*', (req, res, next) => {
-//   req.protocol === 'http' && process.env.NODE_ENV === 'production' ?
-//     res.redirect('https://' + req.hostname + req.url) :
-//     next();
-// });
+app.get('*', (req, res, next) => {
+  req.protocol === 'http' && process.env.NODE_ENV === 'production' ?
+    res.redirect('https://' + req.hostname + req.url) :
+    next();
+});
 
 app.use(express.static('dist'));
-
-app.use(((req, res) => res.send()));
 
 app.listen(process.env.PORT || 3000);
