b6e3ca25952d8b7e4d0589f1865268c03dcac6a0
Rishi Patel <Rishi.Patel@LIBERTYMUTUAL.COM>
2018-06-15 10:34:19 -0400

    landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText, Input } from 'mdbreact';
 
-const Landing = () => <h1>Landing</h1>;
+export default class CardList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-export default Landing;
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <Card>
+                                <CardBody>
+                                    <CardImage className="img-fluid" src="https://nc3t.com/wp-content/uploads/2016/08/boaty.jpg" />
+                                    <CardTitle>Welcome</CardTitle>
+
+                                    <Input label="Username" value={this.state.value} onChange={this.handleChange}/>
+                                    <Button href={`/login/${this.state.value}`}>Login</Button>
+                                    <Button href="/register">Register</Button>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+        );
+    }}
+
